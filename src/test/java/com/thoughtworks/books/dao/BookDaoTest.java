c8a2862781fddf219909f83b88a531a423339c5a
hchauke <hchauke@thoughtworks.com>
2016-06-29 13:02:52 +0200

    Hlulani: completed service layer tests for add ana get books

--- src/test/java/com/thoughtworks/books/dao/BookDaoTest.java
+++ src/test/java/com/thoughtworks/books/dao/BookDaoTest.java
@@ -1,13 +1,15 @@
 package com.thoughtworks.books.dao;
 
 
-import com.thoughtworks.books.dao.BookDAO;
 import com.thoughtworks.books.entity.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,45 +20,43 @@ import java.util.List;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring-servlet.xml")
 @Transactional
+@Rollback(true)
 public class BookDaoTest {
-
-    private Book book;
-
-    private final String BOOK_NAME = "Java";
-    private final String BOOK_ISBN = "boo-101";
-    private final String BOOK_DESCRIPTION = "learn the fundamentals of OOP in java";
-    private final BigDecimal BOOK_PRICE = new BigDecimal(123);
 
     @Autowired
     private BookDAO bookDAO;
 
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    private Book book;
+
+    private Session session;
+
     @Before
     public void setUp() {
 
-        book = new Book();
-
-        book.setName(BOOK_NAME);
-        book.setIsbn(BOOK_ISBN);
-        book.setDescription(BOOK_DESCRIPTION);
-        book.setPrice(BOOK_PRICE);
+        session = sessionFactory.getCurrentSession();
+        book = new Book("Java", "book-101", "Learn java fundamentals in 21 days", new BigDecimal(123));
     }
 
     @Test
-    public void testBookAndItElementsNotNull() throws Exception {
-        //test book not null
-        Assert.assertNotNull(book);
-
-        //test book element not null
-        Assert.assertEquals(BOOK_NAME, book.getName());
-        Assert.assertEquals(BOOK_ISBN, book.getIsbn());
-        Assert.assertEquals(BOOK_DESCRIPTION, book.getDescription());
-        Assert.assertEquals(BOOK_PRICE, book.getPrice());
-    }
-
-    @Test
-    public void testAddAndGetBook() throws Exception {
+    public void testAddBook() throws Exception {
+        session.beginTransaction();
+        Assert.assertEquals(0, session.createQuery("from Book").list().size());
 
         bookDAO.addBook(book);
+        Assert.assertEquals(1, session.createQuery("from Book").list().size());
+
+    }
+
+    @Test
+    public void testGetBooks() throws Exception {
+        Assert.assertEquals(0, bookDAO.getBooks().size());
+
+        bookDAO.addBook(book);
+
+        Assert.assertEquals(1, bookDAO.getBooks().size());
 
         List<Book> bookList = bookDAO.getBooks();
 
@@ -64,5 +64,6 @@ public class BookDaoTest {
         Assert.assertEquals(book.getIsbn(), bookList.get(0).getIsbn());
         Assert.assertEquals(book.getDescription(), bookList.get(0).getDescription());
         Assert.assertEquals(book.getPrice(), bookList.get(0).getPrice());
+
     }
 }
--- src/test/java/com/thoughtworks/books/service/BookServiceTest.java
+++ src/test/java/com/thoughtworks/books/service/BookServiceTest.java
@@ -2,97 +2,54 @@ package com.thoughtworks.books.service;
 
 import com.thoughtworks.books.dao.BookDAO;
 import com.thoughtworks.books.entity.Book;
-import com.thoughtworks.books.service.BookService;
+import com.thoughtworks.books.service.impl.BookServiceImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.fail;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.*;
 
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring-servlet.xml")
-@Transactional
 public class BookServiceTest {
 
-    private Book book;
-
-    private final String BOOK_NAME = "Java";
-    private final String BOOK_ISBN = "boo-101";
-    private final String BOOK_DESCRIPTION = "learn the fundamentals of OOP in java";
-    private final BigDecimal BOOK_PRICE = new BigDecimal(123);
-
-    @Autowired
-    private BookService bookService;
-
+    @Mock
     private BookDAO bookDAO;
+
+    @Spy
+    private List<Book> bookList = new ArrayList<Book>();
+
+    @InjectMocks
+    private BookService bookService = new BookServiceImpl();
 
 
     @Before
     public void setUp() {
-
-        bookDAO = Mockito.mock(BookDAO.class);
-
-        book = new Book();
-
-        book.setName(BOOK_NAME);
-        book.setIsbn(BOOK_ISBN);
-        book.setDescription(BOOK_DESCRIPTION);
-        book.setPrice(BOOK_PRICE);
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
-    public void testBookListEqualZero() {
+    public void testAddOneBook() throws Exception {
+        doNothing().when(bookDAO).addBook(any(Book.class));
+        bookService.addBook(any(Book.class));
+        verify(bookDAO, atLeastOnce()).addBook(any(Book.class));
 
-      try {
-          List<Book> bookList = bookService.getBooks();
-          Assert.assertEquals(0 , bookList.size());
-
-      } catch (Exception ex){
-          ex.printStackTrace();
-          fail("Could not test book equal to zero");
-      }
     }
 
     @Test
-    public void testBookListEqualOne() {
-        try {
-            bookService.addBook(book);
+    public void testGetAllBooks() throws Exception {
+        when(bookDAO.getBooks()).thenReturn(bookList);
 
-            List<Book> bookList = bookService.getBooks();
-            Assert.assertEquals(1 , bookList.size());
-
-        } catch (Exception ex){
-            ex.printStackTrace();
-            fail("Could not test book equal to one");
-        }
+        Assert.assertEquals(bookService.getBooks(), bookList);
+        bookService.getBooks();
+        verify(bookDAO, atLeastOnce()).getBooks();
     }
 
-    @Test
-    public void testAddBookReturnBook() throws Exception {
-
-        try {
-            bookService.addBook(book);
-
-            List<Book> bookList = bookService.getBooks();
-
-            Assert.assertEquals(book.getName(), bookList.get(0).getName());
-            Assert.assertEquals(book.getIsbn(), bookList.get(0).getIsbn());
-            Assert.assertEquals(book.getDescription(), bookList.get(0).getDescription());
-            Assert.assertEquals(book.getPrice(), bookList.get(0).getPrice());
-
-        } catch (Exception ex){
-            ex.printStackTrace();
-            fail("Could not test add book return book");
-        }
-    }
 }
